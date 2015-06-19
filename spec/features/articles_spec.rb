2171e7d0fffdd996e26d99ae2be4ebb7109ccc38
Jay Hayes <ur@iamvery.com>
2015-06-19 10:09:38 -0700

    Add creation test, make it green

--- spec/features/articles_spec.rb
+++ spec/features/articles_spec.rb
@@ -12,4 +12,16 @@ RSpec.feature "articles management", js: true do
       expect(page).to have_content("Infamous Cats")
     end
   end
+
+  describe "article creation" do
+    it "accepts user input to create a new article" do
+      visit articles_path
+      click_on "New Article"
+      fill_in "Title", with: "It begins..."
+      click_on "Create Article"
+      visit articles_path
+
+      expect(page).to have_content("It begins...")
+    end
+  end
 end
